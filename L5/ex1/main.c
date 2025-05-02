986a75d73282abc5983843cd024a68c3808d866f
Amanda Bialer <amandabialer@gmail.com>
2025-05-01 21:43:41 -0300

    execise 1 python

---
+++ L5/ex1/main.c
@@ -0,0 +1,23 @@
+/*
+ * Enunciado:
+ *  Desenvolva um programa capaz de ler uma string digitada pelo usu�rio e,
+ *  a seguir, exiba essa string invertida.
+ *
+ *  Exemplo:
+ *    Digite uma string: ola
+ *    alo
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100 // Valor m�ximo para o tamanho do array. N�o modifique.
+
+/* Prot�tipos */
+
+int main (int argc, char *argv[]){
+  
+  return 0;
+}
